7d0a8bb88008820e365533a4bc0b344b0064d7f9
Dillon Chanis <dillonchanis92@gmail.com>
2018-04-29 16:43:19 -0400

    Creating base input component

---
+++ src/components/input/index.js
@@ -0,0 +1,7 @@
+import BansheeInput from './src/input'
+
+BansheeInput.install = function (Vue) {
+  Vue.component(BansheeInput.name, BansheeInput)
+}
+
+export default BansheeInput
---
+++ src/components/input/src/input.vue
@@ -0,0 +1,31 @@
+<template>
+  <input :type="type"
+    :value="value"
+    v-on="listeners" />
+</template>
+
+<script>
+export default {
+  name: 'BansheeInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  computed: {
+    listeners () {
+      console.log('listener', this.$listeners)
+      return {
+        ...this.$listeners,
+        blur: e => this.$emit('blur', e.target.value),
+        input: e => this.$emit('input', e.target.value)
+      }
+    }
+  }
+}
+</script>
