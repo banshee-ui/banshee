2a49a452222ada6afd13063cef2db5bace2098dd
Dillon Chanis <dillonchanis92@gmail.com>
2018-05-04 08:13:02 -0400

    Creating Banshee tab component

---
+++ src/components/tabs/index.js
@@ -0,0 +1,7 @@
+import BansheeTabs from './src/Tabs'
+
+BansheeTabs.install = function (Vue) {
+  Vue.component(BansheeTabs.name, BansheeTabs)
+}
+
+export default BansheeTabs
---
+++ src/components/tabs/src/Tabs.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: 'BansheeTabs',
+  props: {
+    defaultSelected: {
+      type: String
+    },
+    selected: {
+      type: String,
+      default: undefined
+    },
+    tabs: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data: () => ({
+    internalSelected: ''
+  }),
+  computed: {
+    activeTabPane () {
+      return this.$slots[this.getSelected.toLowerCase()]
+    },
+    getSelected () {
+      return this.isControlled ? this.selected : this.internalSelected
+    },
+    isControlled () {
+      return this.selected !== undefined
+    }
+  },
+  created () {
+    this.internalSelected = this.defaultSelected
+  },
+  methods: {
+    select (tab) {
+      this.internalSelected = tab
+      this.$emit('tabSelect', tab)
+    }
+  },
+  render (h, context) {
+    return h('div', [
+      this.$scopedSlots.default({
+        tabs: this.tabs,
+        select: this.select,
+        selected: this.getSelected
+      }),
+      this.activeTabPane
+    ])
+  }
+}
+</script>
