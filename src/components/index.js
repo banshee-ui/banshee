305375a5c7069fc3e3752ca6fa1701e72045f152
Dillon Chanis <dillonchanis92@gmail.com>
2018-06-12 20:47:45 -0400

    Exporting all components to index file

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import { BansheeCarousel, BansheeCarouselSlide } from './Carousel'
+import BansheeErrorBoundary from './ErrorBoundary'
+import { BansheeExpandable, BansheeExpandableItem, BansheeExpandableTitle, BansheeExpandableBody } from './Expandable'
+import BansheeFetch from './Fetch'
+import { BansheeListbox, BansheeListboxItem } from './Listbox'
+import BansheeMediaQuery from './MediaQuery'
+import BansheeSimpleList from './SimpleList'
+import BansheeSmartList from './SmartList'
+import BansheeStorage from './Storage'
+import { BansheeTabs, BansheeTabList, BansheeTab, BansheeTabPanes, BansheeTabPane } from './Tabs'
+import BansheeToggle from './Toggle'
+
+export {
+  BansheeCarousel,
+  BansheeCarouselSlide,
+  BansheeErrorBoundary,
+  BansheeExpandable,
+  BansheeExpandableItem,
+  BansheeExpandableTitle,
+  BansheeExpandableBody,
+  BansheeFetch,
+  BansheeListbox,
+  BansheeListboxItem,
+  BansheeMediaQuery,
+  BansheeSimpleList,
+  BansheeSmartList,
+  BansheeStorage,
+  BansheeTabs,
+  BansheeTabList,
+  BansheeTab,
+  BansheeTabPanes,
+  BansheeTabPane,
+  BansheeToggle
+}
