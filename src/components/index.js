6b1e5bc3cbc7ee259f73a49d0f26c8d20bbb228f
Dillon Chanis <dillonchanis92@gmail.com>
2018-11-16 09:32:06 -0500

    Cleaning up component exports per @adi518 suggestion

--- src/components/index.js
+++ src/components/index.js
@@ -1,34 +1,11 @@
-import { BansheeCarousel, BansheeCarouselSlide } from './Carousel'
-import BansheeErrorBoundary from './ErrorBoundary'
-import { BansheeExpandable, BansheeExpandableItem, BansheeExpandableTitle, BansheeExpandableBody } from './Expandable'
-import BansheeFetch from './Fetch'
-import { BansheeListbox, BansheeListboxItem } from './Listbox'
-import BansheeMediaQuery from './MediaQuery'
-import BansheeSimpleList from './SimpleList'
-import BansheeSmartList from './SmartList'
-import BansheeStorage from './Storage'
-import { BansheeTabs, BansheeTabList, BansheeTab, BansheeTabPanes, BansheeTabPane } from './Tabs'
-import BansheeToggle from './Toggle'
-
-export {
-  BansheeCarousel,
-  BansheeCarouselSlide,
-  BansheeErrorBoundary,
-  BansheeExpandable,
-  BansheeExpandableItem,
-  BansheeExpandableTitle,
-  BansheeExpandableBody,
-  BansheeFetch,
-  BansheeListbox,
-  BansheeListboxItem,
-  BansheeMediaQuery,
-  BansheeSimpleList,
-  BansheeSmartList,
-  BansheeStorage,
-  BansheeTabs,
-  BansheeTabList,
-  BansheeTab,
-  BansheeTabPanes,
-  BansheeTabPane,
-  BansheeToggle
-}
+export { BansheeCarousel, BansheeCarouselSlide } from './Carousel'
+export { default as BansheeErrorBoundary } from './ErrorBoundary'
+export { BansheeExpandable, BansheeExpandableItem, BansheeExpandableTitle, BansheeExpandableBody } from './Expandable'
+export { default as BansheeFetch } from './Fetch'
+export { BansheeListbox, BansheeListboxItem } from './Listbox'
+export { default as BansheeMediaQuery } from './MediaQuery'
+export { default as BansheeSimpleList } from './SimpleList'
+export { default as BansheeSmartList } from './SmartList'
+export { default as BansheeStorage } from './Storage'
+export { BansheeTabs, BansheeTabList, BansheeTab, BansheeTabPanes, BansheeTabPane } from './Tabs'
+export { default as BansheeToggle } from './Toggle'
